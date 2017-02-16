8a2e16415c00699018e486bc92c506564125b5a9
tbobarino <hopmailkins@gmail.com>
2017-02-16 14:00:42 -0800

    removing large commented out section from app.py

--- project/__init__.py
+++ project/__init__.py
@@ -24,7 +24,7 @@ db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
 from project.messages.views import messages_blueprint
-# register our blueprints with the application
+ 
 app.register_blueprint(users_blueprint, url_prefix='/users')
 app.register_blueprint(messages_blueprint, url_prefix='/users/<int:user_id>/messages')
 
